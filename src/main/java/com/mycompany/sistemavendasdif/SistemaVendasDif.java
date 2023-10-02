41c517b558383a526a5f49efbb580ab12964990b
ARTHUR ANTONIO DE ARAUJO <arthur.7923@aluno.pr.senac.br>
2023-10-02 17:04:11 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/sistemavendasdif/SistemaVendasDif.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemavendasdif;
+
+/**
+ *
+ * @author arthur.7923
+ */
+public class SistemaVendasDif {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
